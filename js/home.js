455059095f732b3f5e0cbd9b1f63d5f56e4e395c
Littlaa <monadags@gmail.com>
2022-05-18 17:11:39 +0200

    Added API

--- js/home.js
+++ js/home.js
@@ -1,3 +1,21 @@
 const url = "https://littlaa.one/projectexam1/wp-json/wp/v2/posts?acf_format=standard&per_page=20";
 
-//const carousellResults =
+const carousellResults = document.querySelector(".resultsCarousell");
+
+async function getPosts() {
+  try {
+    const response = await fetch(url);
+    const posts = await response.json();
+    console.log(posts);
+
+    for (let i = 0; i < posts.length; i++) {
+      carousellResults.innerHTML += `${posts[i].title.rendered}
+                                    <img src="${posts[i].acf.image}">
+                                    `;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getPosts(url);
