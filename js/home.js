c2b19fe9fd0c4ef0dc86f2de29ec466dac6101c4
Littlaa <monadags@gmail.com>
2022-05-20 10:58:01 +0200

    Fixed typo

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 const url = "https://littlaa.one/projectexam1/wp-json/wp/v2/posts?acf_format=standard&per_page=20";
 
-const carousellResults = document.querySelector(".resultsCarousell");
+const carouselResults = document.querySelector(".resultsCarousel");
 
 async function getPosts() {
   try {
@@ -9,7 +9,7 @@ async function getPosts() {
     console.log(posts);
 
     for (let i = 0; i < posts.length; i++) {
-      carousellResults.innerHTML += `<div>
+      carouselResults.innerHTML += `<div>
                                       <a href="specific.html?postsId=${posts[i].id}">
                                     <div>${posts[i].title.rendered}</div>
                                     <img src="${posts[i].acf.image}">
