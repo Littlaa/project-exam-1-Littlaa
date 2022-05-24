de8abf51dff53f3d3415a4332317b0aee91681e0
Littlaa <83633992+Littlaa@users.noreply.github.com>
2022-05-24 17:16:21 +0200

    Added carousel

--- js/home.js
+++ js/home.js
@@ -1,25 +1,73 @@
-const url = "https://littlaa.one/projectexam1/wp-json/wp/v2/posts?acf_format=standard&per_page=20";
+const url =
+  "https://littlaa.one/projectexam1/wp-json/wp/v2/posts?acf_format=standard&per_page=20";
 
 const carouselResults = document.querySelector(".resultsCarousel");
+const paginationElement = document.querySelector(".pageNumbers");
+
+let current_page = 0;
+let rows = 5;
 
 async function getPosts() {
   try {
     const response = await fetch(url);
     const posts = await response.json();
-    console.log(posts);
+    const pageCount = Math.ceil(posts.length / rows);
 
-    for (let i = 0; i < posts.length; i++) {
-      carouselResults.innerHTML += `<div class="containerPosts">
-                                      <a href="specific.html?postsId=${posts[i].id}">
-                                    <div>${posts[i].title.rendered}</div>
-                                    <img src="${posts[i].acf.image}">
-                                    </a>
-                                    </div>
-                                    `;
+    displayPosts(posts, carouselResults, rows, current_page, pageCount);
+
+    function setupPagination(wrapper, rows_per_page) {
+      let leftBtn = paginationButton("<");
+      wrapper.appendChild(leftBtn);
+      let rightBtn = paginationButton(">");
+      wrapper.appendChild(rightBtn);
+      leftBtn.addEventListener("click", function () {
+        displayPosts(posts, carouselResults, rows, --current_page, pageCount);
+      });
+
+      rightBtn.addEventListener("click", function () {
+        displayPosts(posts, carouselResults, rows, ++current_page, pageCount);
+      });
     }
+
+    function paginationButton(page, items) {
+      let button = document.createElement("button");
+      button.innerText = page;
+
+      if (current_page == page) button.classList.add("active");
+
+      return button;
+    }
+
+    setupPagination(paginationElement, rows);
+
+    return posts;
   } catch (error) {
     console.log(error);
   }
 }
 
-getPosts(url);
+getPosts();
+
+function displayPosts(items, wrapper, rows_per_page, page, pageCount) {
+  wrapper.innerHTML = "";
+
+  if (page < 0) {
+    current_page = pageCount - 1;
+  }
+
+  let currentPage = current_page % pageCount;
+  let start = rows_per_page * currentPage;
+  let end = start + rows_per_page;
+  let paginatedItems = items.slice(start, end);
+
+  for (let i = 0; i < paginatedItems.length; i++) {
+    let item = paginatedItems[i];
+    carouselResults.innerHTML += `<div class="containerPosts">
+                                    <a href="specific.html?postsId=${item.id}">
+                                    <div>${item.title.rendered}</div>
+                                    <img src="${item.acf.image}">
+                                    </a>
+                                    </div>
+                                    `;
+  }
+}
