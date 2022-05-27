d1d389d5e51381f1b8fdfcef2a95f977cc09eb23
Littlaa <monadags@gmail.com>
2022-05-27 23:59:38 +0200

    Added modal to images specific

--- js/specific.js
+++ js/specific.js
@@ -13,12 +13,35 @@ async function callDetails() {
 
     detailContainer.innerHTML += `<div class="content">
                                   <h1>${details.title.rendered}</h1>
-                                  <img src="${details.acf.image}" alt="${details.acf.alt}">
+                                  <img src="${details.acf.image}" alt="${details.acf.alt}" id="modalClick">
                                   </div> 
                                   <div class="details">
                                   ${details.acf.description}
                                   </div>
                                   `;
+
+    const modal = document.getElementById("simpleModal");
+    const clickModal = document.getElementById("modalClick");
+    const closeBtn = document.getElementsByClassName("close_btn")[0];
+
+    clickModal.addEventListener("click", openModal);
+    closeBtn.addEventListener("click", closeModal);
+    window.addEventListener("click", outsideClick);
+
+    function openModal() {
+      modal.style.display = "grid";
+      modal.innerHTML = `<img src="${details.acf.image}" alt="${details.acf.alt}" class="modal_content"></img>`;
+    }
+
+    function closeModal() {
+      modal.style.display = "none";
+    }
+
+    function outsideClick(e) {
+      if (e.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   } catch (error) {
     console.log(error);
   }
